224038878c4321520f9eeb29c47f11a688c33093
smuranjan1 <smyan.muranjan@gmail.com>
2024-07-07 14:05:43 -0500

    Working better?

--- src/App.js
+++ src/App.js
@@ -2,44 +2,20 @@ import './App.css';
 import { useState } from 'react';
 import  CalculatorButton  from './CalculatorButton'
 
-const validButtons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '%', '/', '*', '-', '+', '=', '.'];
+const operators = ['+', '-', '*', '/'];
 
 function App() {
 
   const [input, setInput] = useState("0");
   const [result, setResult] = useState([]);
   
-  const handleInput = (value) => (event) => {
-    if (input === "0") {
-      setInput(value)
-    } else {
-      setInput(input.concat(value))
-    }
-  }
-
-  const handleKeydown = (value) => {
-    console.log(value)
-    if (input === "Enter") {
-      calculateResult();
-      return;
-    }
-    if (!validButtons.includes(value)) {
-      return;
-    }
-
-    if (input === "0") {
-      setInput(value)
-    } else {
-      setInput(input.concat(value))
-    }
-  }
-
   const calculateResult = () => {
     try {
-      if (input[0] === '/' || input[0] === '*' || input[0] === '+' || input[0] === '-') {
-        setResult(eval(result + input))
+      const formula = input.replace(/x/g, "*").replace(/÷/g, "/");
+      if (operators.includes(formula[0])) {
+        setResult(eval(result + formula))
       } else {
-        setResult(eval(input));
+        setResult(eval(formula));
       }
     } catch (error) {
       setResult("ERROR")
@@ -47,12 +23,19 @@ function App() {
     setInput("0");
   }
 
+  const handleInput = (value) => (event) => {
+    if (input === "0") {
+      setInput(value)
+    } else {
+      setInput(input => input.concat(value));
+    }
+  }
+
+
   const clearInput = () => {
     setInput("0");
     setResult("");
   }
-
-  // document.addEventListener('keydown', (event) => handleKeydown(event.key));
 
   return (
     <div id="calculator">
@@ -63,11 +46,11 @@ function App() {
       <div id="button-container">
         <button id="clear" onClick={clearInput}>AC</button>
         <CalculatorButton buttonId="percent" buttonText="%" handleClick={handleInput}/>
-        <CalculatorButton buttonId="divide" buttonText="/" handleClick={handleInput}/>
+        <CalculatorButton buttonId="divide" buttonText="÷" handleClick={handleInput}/>
         <CalculatorButton buttonId="seven" buttonText="7" handleClick={handleInput}/>
         <CalculatorButton buttonId="eight" buttonText="8" handleClick={handleInput}/>
         <CalculatorButton buttonId="nine" buttonText="9" handleClick={handleInput}/>
-        <CalculatorButton buttonId="multiply" buttonText="*" handleClick={handleInput}/>
+        <CalculatorButton buttonId="multiply" buttonText="x" handleClick={handleInput}/>
         <CalculatorButton buttonId="four" buttonText="4" handleClick={handleInput}/>
         <CalculatorButton buttonId="five" buttonText="5" handleClick={handleInput}/>
         <CalculatorButton buttonId="six" buttonText="6" handleClick={handleInput}/>
